ec5f8d205f6f510de559b40959ada8413ac991ad
Glitch (artist-explorer) <none>
2020-03-05 22:04:39 +0000

    👖😄 Checkpoint ./package.json:12492470/998 ./server/requirements.txt:12492470/4 ./server/server.py:12492470/56

--- server/server.py
+++ server/server.py
@@ -25,7 +25,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 cors = CORS(app)
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
  
 genres = {
     "genres": [ "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music" ]
@@ -50,7 +50,7 @@ def cached(timeout=5 * 60, key='view/%s'):
 @app.route('/api/genres/<genre_name>/artists')
 @cached(timeout=30 * 60)
 def get_genre_artists(genre_name):
-    response = sp.recommendations(seed_genres=genre_name, limit=50)
+    response = spotify.recommendations(seed_genres=genre_name, limit=50)
     return jsonify(response)
 
 @app.route('/api/genres')
@@ -60,24 +60,24 @@ def get_all_genres():
 
 @app.route('/spotify/artists/<artist_id>')
 def get_artist(artist_id):
-    response = sp.artist(artist_id)
+    response = spotify.artist(artist_id)
     return jsonify(response)
 
 @app.route('/spotify/artists')
 def get_artists():
     ids = request.args.get('ids').split(',')
-    response = sp.artists(ids)
+    response = spotify.artists(ids)
     return jsonify(response)
 
 @app.route('/spotify/artists/<artist_id>/related-artists')
 def get_related_artists(artist_id):
-    response = sp.artist_related_artists(artist_id)
+    response = spotify.artist_related_artists(artist_id)
     return jsonify(response)
 
 @app.route('/spotify/artists/<artist_id>/top-tracks')
 def get_top_tracks(artist_id):
     country = request.args.get('country')
-    response = sp.artist_top_tracks(artist_id, country)
+    response = spotify.artist_top_tracks(artist_id, country)
     return jsonify(response)
 
 
@@ -87,9 +87,9 @@ def search():
     type = request.args.get('type')
     limit = request.args.get('limit')
 
-    response = sp.search(q, type=type, limit=limit)
+    response = spotify.search(q, type=type, limit=limit)
     return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=8000)
